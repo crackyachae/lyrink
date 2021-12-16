5ef4489a75f7afa0c2dcd318fa538a6314af303a
Crackyachae <crackyachae@gmail.com>
2021-12-16 12:02:19 +0900

    Feat: define review model schema

--- src/models/index.js
+++ src/models/index.js
@@ -4,3 +4,4 @@ module.exports.User = require('./user.model');
 module.exports.Album = require('./album.model');
 module.exports.Track = require('./track.model');
 module.exports.Track = require('./lyric.model');
+module.exports.Track = require('./review.model');
