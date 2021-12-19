592f48ce5e1cdba9d10179f005c0caa3a8d568d0
Crackyachae <crackyachae@gmail.com>
2021-12-19 15:21:15 +0900

    Fix: correct exported model name in index

--- src/models/index.js
+++ src/models/index.js
@@ -3,5 +3,5 @@ module.exports.User = require('./user.model');
 
 module.exports.Album = require('./album.model');
 module.exports.Track = require('./track.model');
-module.exports.Track = require('./lyric.model');
-module.exports.Track = require('./review.model');
+module.exports.Lyric = require('./lyric.model');
+module.exports.Review = require('./review.model');
