bd83de57d5e2aaa75fdc7a987ca1d5e2e2df598b
Crackyachae <crackyachae@gmail.com>
2023-01-11 16:18:56 +0900

    chore: edit AppConfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,6 @@
-// FIXME: Update this configuration file based on your project information
-
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: 'Lyrink',
+  title: 'Lyrink',
+  description: 'A music review system focusing on the links between lyrics',
+  locale: 'ko',
 };
