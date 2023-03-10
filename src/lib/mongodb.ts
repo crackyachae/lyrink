8b60f473e589f121cc658ac3227280888dfff790
Crackyachae <crackyachae@gmail.com>
2023-03-11 00:05:55 +0900

    feat: use config file

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,12 +1,11 @@
 /* eslint-disable import/no-mutable-exports */
 import { MongoClient } from 'mongodb';
 
+import config from '@/configs/config';
+
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
-
-const uri = process.env.MONGODB_URI;
-const options = {};
 
 let client;
 let clientPromise: Promise<MongoClient>;
@@ -15,13 +14,13 @@ if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(config.mongoDB.uri, config.mongoDB.options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
+  client = new MongoClient(config.mongoDB.uri, config.mongoDB.options);
   clientPromise = client.connect();
 }
 
