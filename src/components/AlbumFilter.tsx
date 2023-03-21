7acd1e316b9f9544cba992ec2bb8e39cdfa9cdf4
Crackyachae <crackyachae@gmail.com>
2023-03-21 23:47:29 +0900

    feat: implement filter for album

--- src/components/AlbumFilter.tsx
+++ src/components/AlbumFilter.tsx
@@ -1,19 +1,62 @@
-import type { AlbumFilterType } from '@/@types/album';
+import type { Dispatch, SetStateAction } from 'react';
+import { useCallback } from 'react';
+
+import type { AlbumFilterType, AlbumType } from '@/@types/album';
 
 import { AlbumTypeMap } from './constants';
 
-export default function AlbumFilter({ filter }: { filter: AlbumFilterType }) {
-  const albumTypes = Object.values(filter.type);
-  const albumYears = Object.values(filter.year);
+type AlbumFilterProps = {
+  filter: AlbumFilterType;
+  setFilter: Dispatch<SetStateAction<AlbumFilterType>>;
+};
+
+export default function AlbumFilter(props: AlbumFilterProps) {
+  const { filter, setFilter } = props;
+  const albumTypes = Object.keys(filter.type);
+  const albumYears = Object.keys(filter.year);
+
+  const handleClickFilter = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      const { value } = e.currentTarget;
+      const filterType = e.currentTarget.name as keyof AlbumFilterType;
+
+      // TODO: return if filterType is not keyof AlbumFilter
+
+      setFilter((prevFilter) => {
+        // TODO: separate type without condition block
+        let target;
+        if (filterType === 'type') {
+          target = prevFilter.type[value as AlbumType];
+        } else {
+          target = prevFilter.year[value as string];
+        }
+
+        return {
+          ...prevFilter,
+          [filterType]: {
+            ...prevFilter[filterType],
+            [value]: !target,
+          },
+        };
+      });
+    },
+    []
+  );
 
   return (
     <section>
       <div>
         <h4>유형</h4>
         <div>
-          {albumTypes.map((type) => (
-            <button type="button" key={`type-${type}`}>
-              {AlbumTypeMap[type]}
+          {albumTypes.map((albumType) => (
+            <button
+              type="button"
+              key={`type-${albumType}`}
+              name="type"
+              value={albumType}
+              onClick={handleClickFilter}
+            >
+              {AlbumTypeMap[albumType]}
             </button>
           ))}
         </div>
@@ -22,7 +65,13 @@ export default function AlbumFilter({ filter }: { filter: AlbumFilterType }) {
         <h4>연도</h4>
         <div>
           {albumYears.map((year) => (
-            <button type="button" key={`year-${year}`}>
+            <button
+              type="button"
+              key={`year-${year}`}
+              name="year"
+              value={year}
+              onClick={handleClickFilter}
+            >
               {year}
             </button>
           ))}
--- src/pages/artist.tsx
+++ src/pages/artist.tsx
@@ -1,17 +1,33 @@
 /* eslint-disable unused-imports/no-unused-vars */
 
+import { useMemo, useState } from 'react';
+
 import type { Album } from '@/@types/album';
 import AlbumFilter from '@/components/AlbumFilter';
 import AlbumList from '@/components/AlbumList';
 import config from '@/configs/config';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
+import { getAlbumFilter } from '@/utils/filterUtils';
 
-export default function Artist({ albums }: { albums: Album[] }) {
+export default function ArtistPage({ albums }: { albums: Album[] }) {
+  const [filter, setFilter] = useState(getAlbumFilter(albums));
+  const filteredAlbum = useMemo(() => {
+    const isTypeFiltered = Object.values(filter.type).includes(true);
+    const isYearFiltered = Object.values(filter.year).includes(true);
+
+    return albums.filter((album) => {
+      return (
+        (!isTypeFiltered || filter.type[album.albumType]) &&
+        (!isYearFiltered || filter.year[album.releaseDate.slice(0, 4)])
+      );
+    });
+  }, [filter]);
+
   return (
     <Main meta={<Meta title="NELL" description="Lorem ipsum" />}>
-      <AlbumFilter albums={albums} />
-      <AlbumList albums={albums} />
+      <AlbumFilter filter={filter} setFilter={setFilter} />
+      <AlbumList albums={filteredAlbum} />
     </Main>
   );
 }
