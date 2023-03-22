e2c1f100ce346068c81d6b700fd391f6bf861c0e
Crackyachae <crackyachae@gmail.com>
2023-03-22 13:52:12 +0900

    design: use daisy ui for design

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,6 +14,7 @@ module.exports = {
         'test',
         'build',
         'ci',
+        'design',
         'chore',
         'comment',
         'revert',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const daisyui = require('daisyui');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -41,5 +43,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
