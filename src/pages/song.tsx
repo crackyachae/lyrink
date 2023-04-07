008b1ade59ad4d46b0bea62b5dea8f5c5389b10b
Crackyachae <crackyachae@gmail.com>
2023-04-07 14:56:50 +0900

    feat: song page fetch song data with SSR

--- src/pages/song.tsx
+++ src/pages/song.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable unused-imports/no-unused-vars */
 
+import type { GetServerSidePropsContext } from 'next';
+
 import type { TSong } from '@/@types/song';
+import config from '@/configs/config';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
@@ -15,9 +18,18 @@ export default function SongPage({ song }: { song: TSong }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   try {
-    return { props: {} };
+    const { songId } = context.params || { songId: '' };
+
+    if (songId === '') {
+      // TODO: handle error
+    }
+
+    const response = await fetch(`${config.baseUrl}/api/songs/${songId}`);
+    const result = await response.json();
+
+    return { props: { song: result } };
   } catch (e) {
     return {
       // TODO: handle error
