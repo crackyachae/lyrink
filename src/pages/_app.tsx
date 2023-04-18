710924ffcbbc5a8cb8b90e66b53751574a629134
Crackyachae <crackyachae@gmail.com>
2023-04-18 14:00:37 +0900

    feat: use react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/global.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+    <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+  </QueryClientProvider>;
+};
 
 export default MyApp;
