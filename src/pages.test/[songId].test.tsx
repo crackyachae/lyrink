0d8f2906dda6c203a5a16899edbd979588a1b8d2
Crackyachae <crackyachae@gmail.com>
2023-04-25 23:11:00 +0900

    test: add case, render loading or error element
    depends on query status

--- src/pages.test/[songId].test.tsx
+++ src/pages.test/[songId].test.tsx
@@ -2,16 +2,35 @@
  * @jest-environment jsdom
  */
 
+import type { UseQueryResult } from '@tanstack/react-query';
+import { render, screen } from '@testing-library/react';
 import jestFetchMock from 'jest-fetch-mock';
 import type { GetServerSidePropsContext } from 'next';
+import type { NextRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import type { ParsedUrlQuery } from 'querystring';
 
+import type { TSong } from '@/@types/song';
 import config from '@/configs/config';
-import { getServerSideProps } from '@/pages/song/[songId]';
+import { useSongQuery } from '@/hooks/useSong';
+import SongPage, { getServerSideProps } from '@/pages/song/[songId]';
 
 import { Studio1Track2 } from '../../tests/fixtures/song.fixture';
 
 const song = Studio1Track2;
+
+type TUseQueryResultMock<TData, TError> = Partial<
+  UseQueryResult<TData, TError>
+>;
+type TUseRouterMock = Partial<NextRouter>;
+
+jest.mock('@/hooks/useSong', () => ({
+  useSongQuery: jest.fn(),
+}));
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
 
 describe('GIVEN Song page', () => {
   describe('WHEN getServerSideProps works', () => {
@@ -34,6 +53,52 @@ describe('GIVEN Song page', () => {
       );
       // TODO: Need to improve to check dehydrateState in props
       expect(response.props).toHaveProperty('dehydratedState');
+    });
+  });
+
+  const useSongQueryMock = useSongQuery as jest.MockedFunction<
+    (songId: string) => TUseQueryResultMock<TSong, Error>
+  >;
+  const useRouterMock = useRouter as jest.MockedFunction<() => TUseRouterMock>;
+  useRouterMock.mockImplementation(() => ({
+    query: { songId: `${song._id}` } as ParsedUrlQuery,
+  }));
+
+  describe('WHEN song query is loading', () => {
+    beforeEach(() => {
+      useSongQueryMock.mockClear();
+    });
+
+    it('should render a loading element', () => {
+      useSongQueryMock.mockImplementationOnce(() => ({
+        data: undefined,
+        isLoading: true,
+      }));
+
+      render(<SongPage />);
+
+      const loadingElement = screen.getByText(/loading/i);
+      expect(loadingElement).toBeInTheDocument();
+    });
+  });
+
+  describe('WHEN song query results error', () => {
+    it('should render an error message', () => {
+      const error = {
+        name: 'QueryError',
+        message: 'fail to get song data by useQuery',
+      };
+
+      useSongQueryMock.mockImplementationOnce(() => ({
+        isError: true,
+        error,
+      }));
+
+      render(<SongPage />);
+
+      const errorMsgRegex = new RegExp(error.message, 'i');
+      const errorElement = screen.getByText(errorMsgRegex);
+      expect(errorElement).toBeInTheDocument();
     });
   });
 });
