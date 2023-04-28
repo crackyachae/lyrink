3ecafb24729c2827b2c1fc1ea90ae899c46425a7
Crackyachae <crackyachae@gmail.com>
2023-04-28 12:29:44 +0900

    feat: don't use joi

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,38 +1,15 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const Joi = require('joi');
-
-dotenv.config({ path: path.join(__dirname, '../../.env.local') });
-
-const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string()
-      .valid('production', 'development', 'test')
-      .required(),
-    BASE_URL: Joi.string(),
-    MONGODB_URI: Joi.string().required().description('Mongo DB url'),
-    MONGODB_NAME: Joi.string().required().description('Mongo DB database name'),
-  })
-  .unknown();
-
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: 'key' } })
-  .validate(process.env);
-
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
-
 const config = {
-  env: envVars.NODE_ENV,
-  baseUrl: envVars.BASE_URL,
+  env: process.env.NODE_ENV,
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   mongoDB: {
     uri:
-      envVars.NODE_ENV === 'test' ? global.__MONGO_URI__ : envVars.MONGODB_URI,
+      process.env.NODE_ENV === 'test'
+        ? global.__MONGO_URI__
+        : process.env.MONGODB_URI,
     name:
-      envVars.NODE_ENV === 'test'
+      process.env.NODE_ENV === 'test'
         ? global.__MONGO_DB_NAME__
-        : envVars.MONGODB_NAME,
+        : process.env.MONGODB_NAME,
     options: {},
   },
 };
