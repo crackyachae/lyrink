467826da896a55c0bd078805bab121223a7dfc29
Crackyachae <crackyachae@gmail.com>
2023-06-05 21:34:31 +0900

    test: refactor test case
    render albumlist only one time for all test case

--- tests/unit/AlbumList.test.tsx
+++ tests/unit/AlbumList.test.tsx
@@ -2,7 +2,9 @@
  * @jest-environment jsdom
  */
 
-import { render, screen, within } from '@testing-library/react';
+import '@testing-library/react/dont-cleanup-after-each';
+
+import { cleanup, render, screen, within } from '@testing-library/react';
 
 import AlbumList from '@/components/AlbumList';
 import { AlbumTypeMap } from '@/components/constants';
@@ -15,43 +17,67 @@ import {
   StudioTwo,
 } from '../fixtures/album.fixture';
 
-const albums = [EpOne, SingleOne, StudioOne, StudioTwo, StudioThree];
+const singleDiscAlbums = [EpOne, SingleOne, StudioOne, StudioThree];
+const multiDiscAlbums = [StudioTwo];
+const albums = [...singleDiscAlbums, ...multiDiscAlbums];
 
 describe('GIVEN AlbumList component', () => {
   describe('WHEN valid album data has passed', () => {
-    it('THEN it should have all album in list', () => {
-      // TODO: should change query
-      render(<AlbumList albums={albums} />);
+    afterAll(() => {
+      cleanup();
+    });
 
+    render(<AlbumList albums={albums} />);
+
+    it('should have all album in list', () => {
+      // TODO: should change query
       const albumList = screen.getAllByRole('heading');
       expect(albumList).toHaveLength(albums.length);
     });
 
-    it('THEN it should have album information', () => {
-      const album = StudioThree;
-      render(<AlbumList albums={[album]} />);
+    it('should have album information', () => {
+      // select the first album because it's in a definite order (first) in any case
+      const firstAlbum = EpOne;
+      const ALBUM_INDEX = albums.indexOf(firstAlbum);
 
-      const albumTitleRegex = new RegExp(album.title, 'i');
-      const albumTitle = screen.getByRole('heading', { name: albumTitleRegex });
+      const albumTitleRegex = new RegExp(firstAlbum.title, 'i');
+      const albumTitle = screen.getByRole('heading', {
+        name: albumTitleRegex,
+      });
       expect(albumTitle).toBeInTheDocument();
 
-      const albumType = screen.getByText(AlbumTypeMap[album.albumType]);
+      const albumType = screen.getAllByText(AlbumTypeMap[firstAlbum.albumType])[
+        ALBUM_INDEX
+      ];
       expect(albumType).toBeInTheDocument();
 
-      const albumArtist = screen.getByText(album.artists);
+      const albumArtist = screen.getAllByText(firstAlbum.artists)[ALBUM_INDEX];
       expect(albumArtist).toBeInTheDocument();
 
-      const albumReleasedDate = screen.getByText(album.releasedDate);
+      const albumReleasedDate = screen.getAllByText(firstAlbum.releasedDate)[
+        ALBUM_INDEX
+      ];
       expect(albumReleasedDate).toBeInTheDocument();
 
-      const albumCover = screen.getByAltText(albumTitleRegex);
+      const albumCover = screen.getAllByAltText(albumTitleRegex)[ALBUM_INDEX];
       expect(albumCover).toBeInTheDocument();
 
-      const albumSongList = screen.getByRole('table');
+      const albumSongList = screen.getAllByRole('table')[ALBUM_INDEX];
       expect(albumSongList).toBeInTheDocument();
+    });
 
+    it('should have some links to each song page in song list', () => {
+      const album = StudioThree;
+      const ALBUM_INDEX = albums.indexOf(album);
+
+      const albumSongList = screen.getAllByRole('table')[ALBUM_INDEX];
       const validSongLinks =
         album.songs[0]?.filter((song) => song.songId) || [];
+
+      if (!albumSongList) {
+        throw new Error('can not get Song List of given album');
+      }
+
       const albumSongLinks = within(albumSongList).getAllByRole('link');
       expect(albumSongLinks).toHaveLength(validSongLinks.length);
       albumSongLinks.forEach((link, i) => {
@@ -62,17 +88,13 @@ describe('GIVEN AlbumList component', () => {
       });
     });
 
-    it('THEN only albums with two or more disks have a disk row', () => {
-      const albumWithOneDisk = StudioOne;
-      const albumWithTwoDisk = StudioTwo;
-
-      render(<AlbumList albums={[albumWithOneDisk]} />);
-      const albumOneDiscTitle = screen.queryByText(/디스크/i);
-      expect(albumOneDiscTitle).not.toBeInTheDocument();
-
-      render(<AlbumList albums={[albumWithTwoDisk]} />);
-      const albumTwoDiscTitle = screen.getAllByText(/디스크/i);
-      expect(albumTwoDiscTitle).toHaveLength(albumWithTwoDisk.songs.length);
+    it('only albums with two or more disks have a disk row', () => {
+      const multiDiscAlbumDiscTitle = screen.getAllByText(/디스크/i);
+      const multiDiscAlbumDiscCount = multiDiscAlbums.reduce(
+        (acc, album) => acc + album.songs.length,
+        0
+      );
+      expect(multiDiscAlbumDiscTitle).toHaveLength(multiDiscAlbumDiscCount);
     });
   });
 });
